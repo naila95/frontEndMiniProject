ce8848ec9710a705cdeb99abbce0db8828d55b1a
naila95 <naila.ahmedli1995@gmail.com>
2023-10-13 22:02:32 +0400

    home page is ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,23 @@
 window.addEventListener("DOMContentLoaded", function () {
   let tabs = document.querySelectorAll(".tabs div");
   let contents = document.querySelectorAll(".content .content-head");
+  let openFormBtn = document.querySelector(".login-link");
+  let formContainer = document.querySelector(".form-container");
+  let formCloseBtn = document.querySelector(".form-close");
+  let body = document.querySelector("body");
+
+  // FORM
+
+  openFormBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    formContainer.classList.add("show");
+    body.classList.add("filter");
+  });
+
+  formCloseBtn.addEventListener("click", function () {
+    formContainer.classList.remove("show");
+    body.classList.remove("filter");
+  });
 
   for (let tab of tabs) {
     tab.addEventListener("click", function () {
@@ -9,7 +26,6 @@ window.addEventListener("DOMContentLoaded", function () {
       this.classList.add("active");
 
       let index = this.getAttribute("data-index");
-      console.log(index);
       for (let content of contents) {
         if (content.getAttribute("data-index") == index) {
           content.classList.add("show");
@@ -19,4 +35,29 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+
+  // FETCH
+
+  fetch("db.json")
+    .then((res) => res.json())
+    .then((data) => {
+      let html = "";
+      data.products.forEach((product) => {
+        html += `
+        <div class="col-md-3">
+            <div class="item">
+              <span></span>
+              <img class="img-fluid" src="${product.image}" />
+              <div class="item-content">
+                <a class="item-name" href="">${product.name}</a>
+                <p class="item-price">$${product.price}</p>
+                <a class="addToCart" href="#">Add to cart</a>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+
+      document.querySelector(".items-main").innerHTML = html;
+    });
 });
